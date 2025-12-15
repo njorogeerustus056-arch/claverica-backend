1efd0936783a9d070f78b90e95d9da56e07bab9d
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-15 12:29:47 -0800

    Update urls.py

--- escrow/urls.py
+++ escrow/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
-from django.http import JsonResponse
-
-# Temporary test view for escrow API
-def index(request):
-    return JsonResponse({"message": "Escrow API working!"})
+from . import views
 
 urlpatterns = [
-    path('', index, name='escrow_index'),
+    # Base endpoint
+    path('', views.index, name='escrow_index'),
+    
+    # CRUD operations
+    path('create/', views.create_escrow_view, name='create_escrow'),
+    path('list/', views.list_escrows_view, name='list_escrows'),
+    path('<uuid:escrow_id>/', views.get_escrow_detail_view, name='escrow_detail'),
+    path('<uuid:escrow_id>/update/', views.update_escrow_view, name='update_escrow'),
+    
+    # Escrow actions
+    path('<uuid:escrow_id>/fund/', views.fund_escrow_view, name='fund_escrow'),
+    path('<uuid:escrow_id>/release/', views.release_escrow_view, name='release_escrow'),
+    path('<uuid:escrow_id>/dispute/', views.dispute_escrow_view, name='dispute_escrow'),
+    
+    # Messages
+    path('<uuid:escrow_id>/messages/', views.get_messages_view, name='get_messages'),
+    path('<uuid:escrow_id>/send-message/', views.send_message_view, name='send_message'),
+    
+    # Stats
+    path('stats/', views.get_escrow_stats_view, name='escrow_stats'),
 ]
