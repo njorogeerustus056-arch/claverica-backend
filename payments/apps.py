166372d2c81e62f81751f812fc2fc846761d6d4b
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-15 19:36:13 +0300

    Update apps.py

--- payments/apps.py
+++ payments/apps.py
@@ -1,6 +1,11 @@
+# accounts/apps.py
 from django.apps import AppConfig
 
 
-class PaymentsConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'payments'
+    name = 'accounts'
+
+    def ready(self):
+        # Import signals to ensure they are registered
+        import accounts.signals
