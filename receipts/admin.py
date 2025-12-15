7497d226e3aa9bf29097a95e4b75b0d40030fe0b
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-15 11:20:05 -0800

    Update admin.py

--- receipts/admin.py
+++ receipts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Receipt
 
-# Register your models here.
+@admin.register(Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ['original_file_name', 'user_id', 'merchant_name', 'amount', 
+                    'category', 'status', 'uploaded_at']
+    list_filter = ['status', 'category', 'uploaded_at']
+    search_fields = ['user_id', 'original_file_name', 'merchant_name']
+    readonly_fields = ['id', 'uploaded_at', 'updated_at']
+    date_hierarchy = 'uploaded_at'
+    
+    fieldsets = (
+        ('File Information', {
+            'fields': ('id', 'user_id', 'file_name', 'original_file_name', 
+                      'file_size', 'file_type', 'storage_path')
+        }),
+        ('Receipt Details', {
+            'fields': ('merchant_name', 'amount', 'currency', 'transaction_date', 
+                      'category', 'notes', 'tags')
+        }),
+        ('Status', {
+            'fields': ('status',)
+        }),
+        ('Timestamps', {
+            'fields': ('uploaded_at', 'updated_at')
+        }),
+    )
