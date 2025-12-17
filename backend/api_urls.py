1153de31abdc4507236e4d052663eec134103451
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-17 13:39:26 +0300

    Create api_urls.py

---
+++ backend/api_urls.py
@@ -0,0 +1,38 @@
+"""
+API URL Router
+Routes all feature app APIs under /api/<app>/
+"""
+
+from django.urls import path, include
+
+urlpatterns = [
+    # Tasks & Rewards API
+    path('tasks/', include('tasks.urls')),
+    
+    # Accounts API
+    path('accounts/', include('accounts.urls')),
+    
+    # Cards API
+    path('cards/', include('cards.urls')),
+    
+    # Compliance API
+    path('compliance/', include('compliance.urls')),
+    
+    # Crypto API
+    path('crypto/', include('crypto.urls')),
+    
+    # Escrow API
+    path('escrow/', include('escrow.urls')),
+    
+    # Notifications API
+    path('notifications/', include('notifications.urls')),
+    
+    # Payments API
+    path('payments/', include('payments.urls')),
+    
+    # Receipts API
+    path('receipts/', include('receipts.urls')),
+    
+    # Transactions API
+    path('transactions/', include('transactions.urls')),
+]
