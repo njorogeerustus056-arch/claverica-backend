178c712e4170f007f6431ad908eedb0dbfce1d63
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-18 00:45:31 +0300

    Update signals.py

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,8 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Account, Profile
+from .models import Account
 
+# Example: If you still need to do something when an Account is created
 @receiver(post_save, sender=Account)
-def create_profile(sender, instance, created, **kwargs):
+def account_post_save(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        # You can put any initialization code for Account here
+        # For example, logging or setting default values
+        print(f"New Account created: {instance}")
