9a58467f5bcb92b40915f17c56c73aae5128ce7b
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-17 15:37:44 +0300

    Update apps.py

--- Tasks/apps.py
+++ Tasks/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tasks'
+    name = 'Tasks'
     verbose_name = 'Tasks & Rewards'
