0d40f5966d3c1995b4bb6e38425a8b4cf505b206
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-17 13:30:27 +0300

    Update __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,9 @@
+"""
+Claverica Backend API Gateway
+Django fintech backend application
+"""
+
+# This will make Celery work with Django
+# Uncomment if using Celery for async tasks
+# from .celery import app as celery_app
+# __all__ = ('celery_app',)
