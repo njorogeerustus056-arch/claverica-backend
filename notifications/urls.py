0ce3d2a43296e55e8ceb4996029edcc99c5a3fcf
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-18 16:32:34 +0300

    Update urls.py

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,15 +1,21 @@
+# notifications/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+# Create a router and register viewsets
 router = DefaultRouter()
 router.register(r'notifications', views.NotificationViewSet, basename='notification')
 router.register(r'preferences', views.NotificationPreferenceViewSet, basename='notification-preference')
 router.register(r'devices', views.NotificationDeviceViewSet, basename='notification-device')
 router.register(r'templates', views.NotificationTemplateViewSet, basename='notification-template')
 
+# URL patterns
 urlpatterns = [
+    # Include all router-registered viewsets
     path('', include(router.urls)),
+
+    # Function-based API endpoints
     path('send/', views.send_notification, name='send-notification'),
     path('send-bulk/', views.send_bulk_notifications, name='send-bulk-notifications'),
     path('send-template/', views.send_template_notification, name='send-template-notification'),
