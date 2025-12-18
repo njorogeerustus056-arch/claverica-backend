a166952a3f11239caef2527d026961615daa99cb
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-18 15:54:27 +0300

    Update apps.py

--- Tasks/apps.py
+++ Tasks/apps.py
@@ -1,7 +1,14 @@
+# Tasks/apps.py
 from django.apps import AppConfig
 
 
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Tasks'
+    name = 'Tasks'  # matches your folder name
     verbose_name = 'Tasks & Rewards'
+
+    def ready(self):
+        """
+        Import signals or any startup code for the Tasks app here.
+        """
+        import Tasks.signals  # ✅ ensures signals are registered
