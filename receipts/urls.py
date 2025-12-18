a081e91babf3027afc1e160a7260762ae9bb79ac
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2025-12-18 12:11:42 +0300

    Update urls.py

--- receipts/urls.py
+++ receipts/urls.py
@@ -2,20 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Upload
+    # Upload a receipt
     path("upload/", views.upload_receipt_view, name="upload_receipt"),
     
-    # List and filter
+    # List receipts with pagination/filter
     path("list/", views.list_receipts_view, name="list_receipts"),
     
-    # Detail, update, delete
+    # Receipt detail
     path("<uuid:receipt_id>/", views.get_receipt_detail_view, name="receipt_detail"),
+    
+    # Update receipt
     path("<uuid:receipt_id>/update/", views.update_receipt_view, name="update_receipt"),
+    
+    # Delete receipt
     path("<uuid:receipt_id>/delete/", views.delete_receipt_view, name="delete_receipt"),
     
-    # Signed URL
-    path("signed-url/", views.get_signed_url_view, name="get_signed_url"),
-    
-    # Statistics
+    # Receipt stats
     path("stats/", views.get_receipt_stats_view, name="receipt_stats"),
 ]
