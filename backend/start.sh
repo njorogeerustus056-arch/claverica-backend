3337dba99e3622a03e8a9e48f62cce37a0b67b5c
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2026-02-13 08:51:35 -0800

    Fix start.sh syntax error

--- backend/start.sh
+++ backend/start.sh
@@ -5,9 +5,10 @@ echo "PORT: $PORT"
 
 # Run Django check first
 python manage.py check --deploy
+CHECK_RESULT=$?
 
 # If check passes, start Gunicorn
-if [ $? -eq 0 ]; then
+if [ $CHECK_RESULT -eq 0 ]; then
     echo "Django check passed, starting Gunicorn"
     exec gunicorn backend.wsgi:application \
         --bind 0.0.0.0:$PORT \
@@ -17,6 +18,6 @@ if [ $? -eq 0 ]; then
         --access-logfile - \
         --error-logfile -
 else
-    echo "Django check failed!"
+    echo "Django check failed with code $CHECK_RESULT"
     exit 1
 fi
