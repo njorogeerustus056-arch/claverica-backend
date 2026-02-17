510d8604c3148b53d692f13b63c0e82080ac9646
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2026-02-17 14:54:33 -0800

    Fix: Recreate urls.py with PowerShell UTF8 encoding

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+﻿from django.urls import path, include
 from django.http import HttpResponse
 from django.contrib import admin
 from django.views.decorators.http import require_GET
