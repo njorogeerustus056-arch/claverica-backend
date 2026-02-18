a5dbdf250e2b8422614a0738b08c59eea3c49492
njorogeerustus056-arch <njorogeerustus056@gmail.com>
2026-02-18 13:26:49 -0800

    Fix: Update pusher import path for Railway structure

--- backend/urls.py
+++ backend/urls.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 import json
 
-from .views.pusher_auth import pusher_authentication
+from views.pusher_auth import pusher_authentication  # Remove the dot
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 @csrf_exempt
